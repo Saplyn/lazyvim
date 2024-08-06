e86cef532b7f71c41b89f9adeb5817f0e7206884
Qisheng Liu <u7905146@anu.edu.au>
2024-08-06 14:12:01 +1000

    enable lazy extra: mini-surround & yanky

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,5 @@
 -- Add any additional keymaps here
 
 -- LYN: Quicker tab switch
-vim.keymap.set("n", "<leader>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+-- vim.keymap.set("n", "<leader>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+-- vim.keymap.set("n", "<leader>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
