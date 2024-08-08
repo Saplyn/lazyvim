861acd48e066fef6732cc8d4747e572f68756411
Qisheng Liu <u7905146@anu.edu.au>
2024-08-08 16:10:24 +1000

    fix package-info.nvim color display

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -5,7 +5,20 @@ return {
   opts = {
     colors = {
       up_to_date = "#0DB9D7",
+      outdated = "#d19a66",
+    },
+    icons = {
+      style = {
+        up_to_date = "   ",
+        outdated = "   ",
+      },
     },
     package_manager = "npm",
   },
+  config = function(_, opts)
+    require("package-info").setup(opts)
+
+    vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
+    vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
+  end,
 }
