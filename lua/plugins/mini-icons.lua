d437b0a6410a1093b3109af3a3775ec0a3f74bac
Saplyn Miao <saplynx@outlook.com>
2024-08-12 03:44:31 +1000

    feat: dir icon for neo-tree

---
+++ lua/plugins/mini-icons.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      ["package-lock.json"] = { glyph = "", hl = "MiniIconsGreen" },
+      ["tailwind.config.js"] = { glyph = "󱏿", hl = "MiniIconsAzure" },
+      ["Cargo.toml"] = { glyph = "󰏗", hl = "MiniIconsOrange" },
+      ["Cargo.lock"] = { glyph = "󰏗", hl = "MiniIconsOrange" },
+    },
+    directory = {},
+    default = {
+      directory = { hl = "MiniIconsBlue" },
+    },
+  },
+}
