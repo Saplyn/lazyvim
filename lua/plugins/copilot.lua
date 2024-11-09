57e6d741511e3e83568777c65c6abed3c4102d90
Saplyn Miao <saplynx@outlook.com>
2024-11-09 14:22:26 +1100

    feat: lazyvim update & snacks.nvim copilot toggle

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,30 @@
+local Snacks = require("snacks")
+local copilot_exists = pcall(require, "copilot")
+
+if copilot_exists then
+  Snacks.toggle({
+    name = "Copilot Completion",
+    color = {
+      enabled = "green",
+      disabled = "orange",
+    },
+    get = function()
+      return not require("copilot.client").is_disabled()
+    end,
+    set = function(state)
+      if state then
+        require("copilot.command").enable()
+      else
+        require("copilot.command").disable()
+      end
+    end,
+  }):map("<leader>at")
+end
+
 return {
   "zbirenbaum/copilot.lua",
-  keys = {
-    {
-      "<leader>at",
-      function()
-        if require("copilot.client").is_disabled() then
-          require("copilot.command").enable()
-        else
-          require("copilot.command").disable()
-        end
-      end,
-      desc = "Toggle Copilot",
-    },
-  },
   config = function(_, opts)
     require("copilot").setup(opts)
-
     require("copilot.command").disable()
   end,
 }
