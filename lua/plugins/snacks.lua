e2cc85dd16420490247cfe1bc22da114d7f7e5e7
Saplyn Miao <saplynx@outlook.com>
2025-04-09 04:30:32 +1000

    feat: migrate to snacks.explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,23 +1,40 @@
 return {
   "folke/snacks.nvim",
   opts = function(_, opts)
-    local preset_keys = opts.dashboard.preset.keys
-    local quit_entry = preset_keys[#preset_keys]
+    -- LYN: Dashboard
+    local dashboard_preset_keys = opts.dashboard.preset.keys
+    local dashboard_quit_entry = dashboard_preset_keys[#dashboard_preset_keys]
 
-    local keys = {}
-    vim.list_extend(keys, preset_keys, nil, #preset_keys - 1)
+    local dashboard_keys = {}
+    vim.list_extend(dashboard_keys, dashboard_preset_keys, nil, #dashboard_preset_keys - 1)
 
     -- stylua: ignore
-    vim.list_extend(keys, {
+    vim.list_extend(dashboard_keys, {
       { action = 'ene | DBUI',                                                 desc = "Database",           icon = "󰆼 ", key = "D" },
       { action = function() LazyVim.lazygit({ cwd = LazyVim.root.git() }) end, desc = "Lazygit (Root Dir)", icon = "󰊢 ", key = "G" },
-      quit_entry,
+      dashboard_quit_entry,
     })
-    opts.dashboard.preset.keys = keys
+    opts.dashboard.preset.keys = dashboard_keys
 
     for _, button in ipairs(opts.dashboard.preset.keys) do
       button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
       button.key_format = "  %s"
     end
+
+    -- LYN: Explorer
+    opts.picker = {
+      sources = {
+        explorer = {
+          win = {
+            list = {
+              keys = {
+                ["t"] = "confirm",
+                ["<CR>"] = "tab",
+              },
+            },
+          },
+        },
+      },
+    }
   end,
 }
