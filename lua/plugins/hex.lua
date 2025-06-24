989321df6daeade65038666db1530c6066a67eaf
Saplyn Miao <saplynx@outlook.com>
2025-06-25 03:55:20 +0800

    feat: hex.nvim toggle

--- lua/plugins/hex.lua
+++ lua/plugins/hex.lua
@@ -1,4 +1,12 @@
 return {
   "RaafatTurki/hex.nvim",
+  cmd = {
+    "HexDump",
+    "HexAssemble",
+    "HexToggle",
+  },
+  keys = {
+    { "<leader>ch", "<Cmd>HexToggle<CR>", mode = { "n" }, desc = "Toggle Hex Display" },
+  },
   opts = {},
 }
