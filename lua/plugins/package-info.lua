83ab325a32800f034e5af9cdac585271014a7861
Saplyn Miao <saplynx@outlook.com>
2025-10-20 10:33:53 +1100

    fix: migrate to new package-info config spec

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -3,9 +3,9 @@ return {
   dependencies = { "MunifTanjim/nui.nvim" },
   ft = "json",
   opts = {
-    colors = {
-      up_to_date = "#0DB9D7",
-      outdated = "#d19a66",
+    highlights = {
+      up_to_date = { fg = "#0DB9D7" },
+      outdated = { fg = "#d19a66" },
     },
     icons = {
       style = {
